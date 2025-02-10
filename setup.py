79847eef1d306e1cc84d2bd676985835fc0d809d
Firas Tlili <firaseltlili@gmail.com>
2025-02-10 22:01:13 +0100

    Adding local package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Medical AI Chatbot',  # Required 
+    version='1',  # Required
+    author="Firas Tlili",
+    author_email="firaseltlili@gmail.com",
+    packages=find_packages(),  # Required
+    install_requires=[]
+)
